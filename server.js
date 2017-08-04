ba8de502dc6331bd08b68c0847b8a39dfa7b5858
harikrishna5000 <30620721+harikrishna5000@users.noreply.github.com>
2017-08-04 07:39:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,20 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/one', function (req, res) {
+  res.sendFile('o n e');
+});
+
+app.get('/two', function (req, res) {
+  res.sendFile('t w o');
+});
+
+app.get('/three', function (req, res) {
+  res.sendFile('t h r e e ');
+});
+
+
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
